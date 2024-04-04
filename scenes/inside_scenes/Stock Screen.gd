5f52b214283e1c5e05107de5acc1d9b6b79f1cbf
VG-Fish <112359514+VG-Fish@users.noreply.github.com>
2024-04-03 23:29:50 -0400

    stock screen will now only change after a day has passed

--- scenes/inside_scenes/Stock Screen.gd
+++ scenes/inside_scenes/Stock Screen.gd
@@ -1,19 +1,24 @@
 extends Control
 
 
+var lines: PackedVector2Array = PackedVector2Array()
+var colors: PackedColorArray = PackedColorArray()
+var can_change_stocks: bool = true
+
+
+func _ready():
+	Globals.connect("day_changed", change_stocks)
+	
+
+func change_stocks() -> void:
+	can_change_stocks = true
+	
+
 func _draw() -> void:
 	# Getting window size
 	var window_size: Vector2i = get_window().size
 	var window_size_x = window_size.x
 	var window_size_y = window_size.y
-	
-	# Defining colors, green for up, red for down
-	var green: Color = Color.LAWN_GREEN
-	var red: Color = Color.RED
-	
-	# Stock variables
-	var stock_price_last_20_days: Array[float] = Globals.stock_price_last_20_days
-	const initial_stock_price = Globals.initial_stock_price
 	
 	# Drawing borders of stock window
 	var top_x: int = 40
@@ -25,17 +30,27 @@ func _draw() -> void:
 	var graph_outline: Rect2 = Rect2(top_right, bottom_left)
 	draw_rect(graph_outline, Color(1, 1, 1, 1), false, 10)
 	
+	if !can_change_stocks:
+		draw_polyline_colors(lines, colors, 10)
+		return
+	
+	# Defining colors, green for up, red for down
+	var green: Color = Color.LAWN_GREEN
+	var red: Color = Color.RED
+	
 	# drawing stock lines
 	var middle_y = top_y + graph_outline.size.y / 2
 	var current_stock_position: Vector2 = Vector2(top_x, middle_y) 
 	var current_stock_price: float = Globals.stock_price
-	
-	var lines: PackedVector2Array = PackedVector2Array()
-	var colors: PackedColorArray = PackedColorArray()
 	# to make sure line doesn't go out of screen
 	var max_change_y: float = graph_outline.end.y / 35
 
 	var previous_stock_price: float 
+	
+	# Stock variables
+	var stock_price_last_20_days: Array[float] = Globals.stock_price_last_20_days
+	var initial_stock_price: float = Globals.stock_price
+	
 	for i in stock_price_last_20_days:
 		previous_stock_price = current_stock_price
 		current_stock_price -= i
@@ -57,6 +72,8 @@ func _draw() -> void:
 			current_stock_line_color = green
 		colors.append(current_stock_line_color)
 		colors.append(current_stock_line_color)
+	
+	can_change_stocks = false
 	draw_polyline_colors(lines, colors, 10)
 	
 	$"Stock Price".text = ""
@@ -64,6 +81,3 @@ func _draw() -> void:
 	Globals.stock_price = current_stock_price
 	$"Stock Price".text += "Current Share Price: $%s\n" % str(snapped(current_stock_price, 0.01))
 	$"Stock Price".text += "Total Percent Change: %s" % str(snapped((current_stock_price - initial_stock_price) / initial_stock_price, 0.01)) + "%"
-
-
-
